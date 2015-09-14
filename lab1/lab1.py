4bc14ab595a03026a06b3c250ff82ca02635a8e7
Deepak <kumarde@umich.edu>
2015-09-14 12:56:25 -0400

    pushing lab1

---
+++ lab1/lab1.py
@@ -0,0 +1,38 @@
+'''
+    1. Write an implementation of FizzBuzz iteratively. 
+    
+    FizzBuzz takes in an integer n, and for every number from 1 - n,
+    prints "Fizz" if the number is divisible by 3, "Buzz" if it is divisible
+    by 5, and "FizzBuzz" if the number is divisible by 15.
+'''
+
+def FizzBuzz(n):
+    #Your code here
+    pass
+
+
+'''
+    2. Write both an iterative and list comprehension solution to the following:
+    
+    Given a list of integers (myList), print the number * 10 if the number is NOT 
+    a multiple of ten.
+
+'''
+myList = [10, 11, 15, 17, 20, 21, 24, 25, 28, 29, 30, 31, 35, 36, 40, 45, 50]
+
+def IterativeFunc():
+    #Your code here
+    pass
+
+def ListComprehensionFunc():
+    #Your code here
+    pass
+
+'''
+    3. Write a function that prints every three letter permutation of the alphabet
+    using a data structure comprehension. Be focused on doing it pythonically.
+'''
+
+def AlphabetPermuation():
+    #Your code here
+    pass
