844eecccfe145e252d67dbb441f9db1d1d26b693
Deepak <kumarde@umich.edu>
2015-09-21 10:49:58 -0400

    pushing lab2 exercises

---
+++ lab2/lab2.py
@@ -0,0 +1,23 @@
+'''
+    1. Write an implementation of stringToNum 
+    
+    Takes in input string, returns list of 0-indexed values of characters in string
+    in lowercase
+
+    "abcde" -> [0, 1, 2, 3, 4]
+'''
+
+def stringToNum(string_in):
+    pass 
+
+'''
+    2. Write an implementation of numToSTring
+
+    Takes in a list of numbers (0 indexed alphabet characters from 0 - 25)
+    returns a string
+
+    [0, 1, 2, 3, 4] -> "abcde"
+'''
+
+def numToString(nums):
+    pass
