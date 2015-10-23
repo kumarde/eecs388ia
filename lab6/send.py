a470628b2e771d97d1af7e0f3da639ef4b020ed5
Deepak <kumarde@umich.edu>
2015-10-23 12:58:16 -0400

    fix

--- lab6/send.py
+++ lab6/send.py
@@ -1,5 +1,4 @@
 import base64
-
-obfuscated = "aW1wb3J0IHJlcXVlc3RzCmltcG9ydCB0aW1lCgp3aGlsZSBUcnVlOgogICAgciA9IHJlcXVlc3RzLmdldCgiaHR0cDovLzUyLjIzLjIwMS4xODQiKQogICAgdGltZS5zbGVlcCg1KQo="
+obfuscated = "aW1wb3J0IHVybGxpYjIKaW1wb3J0IHRpbWUKCndoaWxlIFRydWU6CiAgICB1cmxsaWIyLnVybG9wZW4oImh0dHA6Ly81Mi4yMy4yMDEuMTg0IikKICAgIHRpbWUuc2xlZXAoNSkK"
 
 eval(compile(base64.b64decode(obfuscated),'<string>','exec'))
